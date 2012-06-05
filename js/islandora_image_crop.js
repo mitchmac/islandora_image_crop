09bda04020481ae937a5e42ac09a0045921d176b
Mitchell MacKenzie <mitch@mitchmac.com>
2012-06-05 00:39:16 -0300

    First working version

--- js/islandora_image_crop.js
+++ js/islandora_image_crop.js
@@ -1,10 +1,12 @@
 jQuery(function($){
-  $('#target').Jcrop(
-	onChange:   updateCoords,
+  $('#jcrop-image').Jcrop({
     onSelect:   updateCoords
-  );
+  });
+  
+  function updateCoords(c) {
+    $("input[name='x']").val(c.x);
+    $("input[name='y']").val(c.y);
+    $("input[name='w']").val(c.w);
+    $("input[name='h']").val(c.h);
+  }
 });
-
-function updateCoords(c) {
-	
-}
